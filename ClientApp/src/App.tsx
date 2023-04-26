2a09f1c5087ac9c7cefbecfd7afca7f9bb694c28
Roman3672 <romka.3672@gmail.com>
2023-04-26 19:51:25 +0300

    Test commit

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Layout from './components/Layout';
 import './styles/custom.scss';
 import TodoList from "./components/TodoList";
@@ -6,7 +5,8 @@ import TodoList from "./components/TodoList";
 const App = () => {
   return(
       <Layout>
-        <TodoList />
+        <TodoList />\
+        dfl;gkdf
       </Layout>
   )
 }
